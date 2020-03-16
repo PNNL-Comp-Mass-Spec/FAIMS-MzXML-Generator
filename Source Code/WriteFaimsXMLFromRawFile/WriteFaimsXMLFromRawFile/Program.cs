fc153b0b53a260f25e0f3c225e41386d40b6cd4f
alchemistmatt <matthew.monroe@pnnl.gov>
2020-03-16 16:52:55 -0700

    Add support for wildcards and show progress messages

--- Source Code/WriteFaimsXMLFromRawFile/WriteFaimsXMLFromRawFile/Program.cs
+++ Source Code/WriteFaimsXMLFromRawFile/WriteFaimsXMLFromRawFile/Program.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using CSMSL;
 using System.IO;
+using System.Reflection;
 using CSMSL.IO.Thermo;
 using System.Security.Cryptography;
+using PRISM;
+using PRISM.FileProcessor;
 
 namespace WriteFaimsXMLFromRawFile
 {
@@ -13,105 +15,283 @@ namespace WriteFaimsXMLFromRawFile
     {
         static void Main(string[] args)
         {
-            var filePath = args[0];
-            var baseOutPath = args[1];         
-            //var filePath = @"C:\Users\dbrademan\Desktop\ReAdW Test\Multi CV Test\20feb2018_60cv_60min_CV_55_70.raw";
-            //var baseOutPath = @"C:\Users\dbrademan\Desktop\ReAdW Test\Multi CV Test";
-
-            //var filePath = @"C:\Users\dbrademan\Desktop\ReAdW Test\Large File\29mar2018_K562_4HR_4CV_6UG_06S_15MS_1.raw";
-            //var baseOutPath = @"C:\Users\dbrademan\Desktop\ReAdW Test\Large File";   
-
-            // quickly hash the raw file before opening it
-            var fileSha1 = HashRawFile(filePath);
-
-            // open up the raw file connection
-            var rawFile = new ThermoRawFile(filePath);
-            rawFile.Open();
-
-            // get all unique CV values from scans
-            var cvValues = GetUniqueCvValues(rawFile);
-            
-
-            // now work for each unique CV value (# files we're going to need to split into)
-            // get all scans that have the CV that we're currently targeting
-            foreach (var cvValue in cvValues)
+            try
             {
-                var targetScans = FindAllTargetScans(cvValue, rawFile);
-                var t = "";
-
-                var outPath = baseOutPath + "\\" + Path.GetFileNameWithoutExtension(filePath) + "_" + cvValue + ".mzxml";
-
-                var writer = new StreamWriter(outPath);
-
-                var header = WriteHeader(writer, filePath, rawFile, fileSha1, targetScans);
-
-                Ms1Scan currentScan = null;
-                // write out our target scans
-                for (int i = 0; i < targetScans.Count; i++)
+                if (args.Length == 0)
                 {
-                    var scan = new MsScan(targetScans[i], rawFile);
+                    var exePath = ProcessFilesOrDirectoriesBase.GetAppPath();
 
-                    if (scan.msLevel == 1)
-                    {
-                        if (currentScan == null)
-                        {
-                            // start condition
-                            currentScan = Ms1Scan.Create(scan);
-                        }
-                        else
-                        {
-                            // write currentScan to file
-                            var outString = currentScan.ToXML();
-                            writer.WriteLine(outString);
-
-                            rawFile.ClearCachedScans();
-                            currentScan = Ms1Scan.Create(scan);
-                        }
-                    }
-                    else if (scan.msLevel == 2)
-                    {
-                        var ms2Scan = Ms2Scan.Create(scan);
-                        ms2Scan.AddMs2ScanParameters(rawFile);
-                        currentScan.AddMs2Scan(ms2Scan);
-                    }
+                    Console.WriteLine("Syntax:");
+                    Console.WriteLine("{0} InstrumentFile.raw [Output_Directory_Path]", exePath);
+                    Console.WriteLine();
+                    Console.WriteLine("Wild cards are also supported, e.g. *.raw");
+                    Console.WriteLine();
+                    // ReSharper disable StringLiteralTypo
+                    Console.WriteLine("Written by Dain Brademan for the Joshua Coon Research Group (University of Wisconsin) in 2018");
+                    Console.WriteLine("Functionality expanded by Matthew Monroe for PNNL (Richland, WA) in 2020");
+                    Console.WriteLine("E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov");
+                    Console.WriteLine("Website: https://omics.pnl.gov/ or https://panomics.pnnl.gov/");
+                    // ReSharper restore StringLiteralTypo
+                    return;
                 }
 
-                // once we're out, we need to write out our last currentScan
-                writer.WriteLine(currentScan.ToXML());
-                currentScan = null;
+                var inputFilePathSpec = args[0];
 
-                //finish off msRun
-                writer.WriteLine(" </msRun>");
-                writer.WriteLine(" <index name=\"scan\" >");
+                string outputDirectoryPath;
 
-                // add special entry to our indexOffset list for where the offsets start
-                var index = new Index(0, ByteVariables.byteDepth + ByteVariables.encoder.GetByteCount(" </msRun>") + 3);
-                ByteVariables.scanOffsets.Add(index);
-
-                // write all index offsets
-                for (int i = 0; i < ByteVariables.scanOffsets.Count - 1; i++)
+                if (args.Length > 1)
                 {
-                    var offset = ByteVariables.scanOffsets[i];
-                    writer.WriteLine("  <offset id=\"" + offset.scanNumber + "\" >" + offset.byteDepth + "</offset>");
+                    outputDirectoryPath = args[1];
+                }
+                else
+                {
+                    outputDirectoryPath = string.Empty;
                 }
 
-                writer.WriteLine(" </index>");
-                writer.WriteLine(" <indexOffset>" + ByteVariables.scanOffsets.Last().byteDepth + "</indexOffset>");
-                //writer.WriteLine(" <sha1>3aaa100a7ac2cef7a798fbbe9973a6f9c994f208</sha1>");
-                writer.Write(" <sha1>");
-                writer.Flush();
-                writer.Close();
-                var mzXmlHash = HashMzXML(writer, outPath);
-                writer = new StreamWriter(outPath, true);
-                writer.Write(mzXmlHash);
-                writer.WriteLine("</sha1>");
-                writer.WriteLine("</mzXML>");
+                ProcessFiles(inputFilePathSpec, outputDirectoryPath);
+            }
+            catch (Exception ex)
+            {
+                ConsoleMsgUtils.ShowError("Error in Program.Main", ex);
+            }
+        }
 
-                writer.Close();
-                writer.Dispose();
-                // reset static variables for next iteration
-                ByteVariables.byteDepth = 0;
-                ByteVariables.scanOffsets.Clear();
+        private static void ProcessFiles(string inputFilePathSpec, string outputDirectoryPath)
+        {
+            if (string.IsNullOrWhiteSpace(inputFilePathSpec))
+            {
+                ConsoleMsgUtils.ShowWarning("Input file path sent to ProcessFiles is empty; unable to continue");
+                return;
+            }
+
+            try
+            {
+
+                // See if inputFilePath contains a wildcard
+                if (inputFilePathSpec.IndexOf('*') >= 0 || inputFilePathSpec.IndexOf('?') >= 0)
+                {
+                    // Obtain a list of the matching files
+
+                    // Copy the path into cleanPath and replace any * or ? characters with _
+                    var cleanPath = inputFilePathSpec.Replace("*", "_").Replace("?", "_");
+
+                    var datasetFile = new FileInfo(cleanPath);
+                    string inputDirectoryPath;
+
+                    if (datasetFile.Directory != null && datasetFile.Directory.Exists)
+                    {
+                        inputDirectoryPath = datasetFile.DirectoryName;
+                    }
+                    else
+                    {
+                        // Use the current working directory
+                        inputDirectoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    }
+
+                    if (string.IsNullOrEmpty(inputDirectoryPath))
+                        inputDirectoryPath = ".";
+
+                    var inputDirectory = new DirectoryInfo(inputDirectoryPath);
+
+                    // Remove any directory information from inputFilePathSpec
+                    inputFilePathSpec = Path.GetFileName(inputFilePathSpec);
+
+                    var matchCount = 0;
+
+                    DirectoryInfo outputDirectory;
+                    if (string.IsNullOrWhiteSpace(outputDirectoryPath))
+                    {
+                        outputDirectory = inputDirectory;
+                    }
+                    else
+                    {
+                        outputDirectory = new DirectoryInfo(outputDirectoryPath);
+                    }
+
+                    Console.WriteLine("Input file spec: {0}", inputFilePathSpec);
+                    Console.WriteLine("Output directory: {0}", outputDirectory.FullName);
+
+                    if (!outputDirectory.Exists)
+                    {
+                        Console.WriteLine("Creating missing output directory");
+                        outputDirectory.Create();
+                    }
+
+                    foreach (var fileItem in inputDirectory.GetFiles(inputFilePathSpec))
+                    {
+                        ProcessFile(fileItem.FullName, outputDirectory.FullName);
+
+                        matchCount += 1;
+
+                        if (matchCount % 100 == 0)
+                            Console.Write(".");
+                    }
+
+                    if (matchCount == 0)
+                    {
+                        ConsoleMsgUtils.ShowWarning("No match was found for the input file path spec:" + inputFilePathSpec);
+                    }
+                }
+                else
+                {
+                    var inputFile = new FileInfo(inputFilePathSpec);
+
+                    DirectoryInfo outputDirectory;
+                    if (string.IsNullOrWhiteSpace(outputDirectoryPath))
+                    {
+                        outputDirectory = inputFile.Directory ?? new DirectoryInfo(".");
+                    }
+                    else
+                    {
+                        outputDirectory = new DirectoryInfo(outputDirectoryPath);
+                    }
+
+                    Console.WriteLine("Input file path: {0}", inputFilePathSpec);
+                    Console.WriteLine("Output directory: {0}", outputDirectory.FullName);
+
+                    if (!outputDirectory.Exists)
+                    {
+                        Console.WriteLine("Creating missing output directory");
+                        outputDirectory.Create();
+                    }
+
+                    ProcessFile(inputFile.FullName, outputDirectory.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                ConsoleMsgUtils.ShowError("Error in ProcessFiles", ex);
+            }
+        }
+
+        private static void ProcessFile(string filePath, string outputDirectoryPath)
+        {
+            try
+            {
+                Console.WriteLine("Processing {0}", filePath);
+
+                // quickly hash the raw file before opening it
+                var fileSha1 = HashRawFile(filePath);
+
+                // open up the raw file connection
+                var rawFile = new ThermoRawFile(filePath);
+                rawFile.Open();
+
+                ConsoleMsgUtils.ShowDebug("Reading CV values");
+
+                // get all unique CV values from scans
+                var cvValues = GetUniqueCvValues(rawFile);
+
+                var totalScanCount = rawFile.GetMsScans().Count();
+                var scansProcessed = 0;
+
+                var lastProgress = DateTime.UtcNow;
+
+                // now work for each unique CV value (# files we're going to need to split into)
+                // get all scans that have the CV that we're currently targeting
+                foreach (var cvValue in cvValues)
+                {
+                    var outPath = outputDirectoryPath + "\\" + Path.GetFileNameWithoutExtension(filePath) + "_" + cvValue + ".mzXML";
+                    ConsoleMsgUtils.ShowDebug("Creating file {0}", outPath);
+
+                    var targetScans = FindAllTargetScans(cvValue, rawFile);
+
+                    using (var writer = new StreamWriter(outPath))
+                    {
+
+                        WriteHeader(writer, filePath, rawFile, fileSha1, targetScans);
+
+                        Ms1Scan currentScan = null;
+
+                        // write out our target scans
+                        foreach (var scanNumber in targetScans)
+                        {
+                            if (DateTime.UtcNow.Subtract(lastProgress).TotalSeconds >= 3)
+                            {
+                                var percentComplete = scansProcessed / (double)totalScanCount * 100;
+
+                                ConsoleMsgUtils.ShowDebugCustom(string.Format("... processing: {0:F0}% complete", percentComplete), emptyLinesBeforeMessage: 0);
+                                lastProgress = DateTime.UtcNow;
+                            }
+
+                            var scan = new MsScan(scanNumber, rawFile);
+
+                            if (scan.msLevel == 1)
+                            {
+                                if (currentScan == null)
+                                {
+                                    // start condition
+                                    currentScan = Ms1Scan.Create(scan);
+                                }
+                                else
+                                {
+                                    // write currentScan to file
+                                    var outString = currentScan.ToXML();
+                                    writer.WriteLine(outString);
+
+                                    rawFile.ClearCachedScans();
+                                    currentScan = Ms1Scan.Create(scan);
+                                }
+                            }
+                            else if (scan.msLevel == 2)
+                            {
+                                var ms2Scan = Ms2Scan.Create(scan);
+                                ms2Scan.AddMs2ScanParameters(rawFile);
+                                currentScan?.AddMs2Scan(ms2Scan);
+                            }
+
+                            scansProcessed++;
+                        }
+
+                        if (currentScan != null)
+                        {
+                            // once we're out, we need to write out our last currentScan
+                            writer.WriteLine(currentScan.ToXML());
+                        }
+
+                        //finish off msRun
+                        writer.WriteLine(" </msRun>");
+
+                        writer.WriteLine(" <index name=\"scan\" >");
+
+                        // add special entry to our indexOffset list for where the offsets start
+                        var index = new Index(0, ByteVariables.byteDepth + ByteVariables.encoder.GetByteCount(" </msRun>") + 3);
+                        ByteVariables.scanOffsets.Add(index);
+
+                        // write all index offsets
+                        for (var i = 0; i < ByteVariables.scanOffsets.Count - 1; i++)
+                        {
+                            var offset = ByteVariables.scanOffsets[i];
+                            writer.WriteLine("  <offset id=\"" + offset.scanNumber + "\" >" + offset.byteDepth + "</offset>");
+                        }
+
+                        writer.WriteLine(" </index>");
+                        writer.WriteLine(" <indexOffset>" + ByteVariables.scanOffsets.Last().byteDepth + "</indexOffset>");
+                        writer.Write(" <sha1>");
+                    }
+
+                    // Compute the SHA-1 hash of the file up to this point
+                    var mzXmlHash = HashMzXML(outPath);
+
+                    // Append the hash
+                    using (var writer = new StreamWriter(outPath, true))
+                    {
+                        writer.Write(mzXmlHash);
+                        writer.WriteLine("</sha1>");
+                        writer.WriteLine("</mzXML>");
+                    }
+
+                    // Reset static variables for next iteration
+                    ByteVariables.byteDepth = 0;
+                    ByteVariables.scanOffsets.Clear();
+                }
+
+                ConsoleMsgUtils.ShowDebugCustom(string.Format("... processing: {0:F0}% complete", 100), emptyLinesBeforeMessage: 0);
+                System.Threading.Thread.Sleep(750);
+            }
+            catch (Exception ex)
+            {
+                ConsoleMsgUtils.ShowError("Error in ProcessFile", ex);
             }
         }
 
