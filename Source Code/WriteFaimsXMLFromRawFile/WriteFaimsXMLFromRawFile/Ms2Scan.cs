f9683b50db14ff183840e0588b92d75b607aa112
alchemistmatt <matthew.monroe@pnnl.gov>
2020-03-16 16:40:31 -0700

    Remove unused references

--- Source Code/WriteFaimsXMLFromRawFile/WriteFaimsXMLFromRawFile/Ms2Scan.cs
+++ Source Code/WriteFaimsXMLFromRawFile/WriteFaimsXMLFromRawFile/Ms2Scan.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using CSMSL;
 using CSMSL.IO.Thermo;
 using System.Text.RegularExpressions;
 
